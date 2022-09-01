f7b918b924a10c6ba73907408e36cf31a7eea888
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2022-09-01 20:00:57 +0300

    v1.0

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 //---	DOM    ---
 let gameboardDOM = document.getElementById("gameBoard");
 let newGameDOM = document.getElementById("newGame");
+let c0 = document.getElementById("c0");
+let c1 = document.getElementById("c1");
+let c2 = document.getElementById("c2");
+let c3 = document.getElementById("c3");
+let c4 = document.getElementById("c4");
+let c5 = document.getElementById("c5");
+let c6 = document.getElementById("c6");
+let c7 = document.getElementById("c7");
+let c8 = document.getElementById("c8");
 //let cellsDOM = document.querySelectorAll(".slot");
 
 //---	OBJECTS    ---
-var gameBoard = {
-  gameboard: [
-    { a1: "" },
-    { a2: "" },
-    { a3: "" },
-    { b1: "" },
-    { b2: "" },
-    { b3: "" },
-    { c1: "" },
-    { c2: "" },
-    { c3: "" },
-  ],
-};
 var createPlayer = (name, marker) => {
   return { name, marker };
 };
-let player1 = createPlayer("Nikos", "O");
-let player2 = createPlayer("Sandra", "X");
-let activePlayer = player1;
+var gameBoard = {
+  theGameboard: ["", "", "", "", "", "", "", "", ""],
+  playerO: createPlayer("Nikos", "O"),
+  playerX: createPlayer("Sandra", "X"),
+};
+//--- GLOBAL VARIABLES  ---
+let activePlayer = gameBoard.playerO;
 let turnCounter = 1;
 //---	MODULES    ---
 
 var menu = (() => {
   var newGame = () => {
     console.log("newGame called");
+    document.getElementById("win_block").textContent = ``;
     turnCounter = 1;
     domControl.clearScreen();
-    gameFlow.round(player1, player2);
+    gameBoard.theGameboard = ["", "", "", "", "", "", "", "", ""];
+    gameboardDOM.style.opacity = "1";
+    gameFlow.round(gameBoard.playerO, gameBoard.playerX);
   };
   return { newGame };
 })();
 var gameFlow = (() => {
   let round = (playerA, playerB) => {
     console.log(`gameFlow turnCounter = ${turnCounter}`);
+
     if (turnCounter % 2 != 0) {
       activePlayer = playerA;
       console.log(
@@ -50,9 +54,50 @@ var gameFlow = (() => {
       );
     }
   };
-  var isValidPosition = (cellId) =>
-    document.getElementById(cellId).textContent == "" ? true : false;
-  return { round, isValidPosition };
+  var isValidPosition = (cellId) => {
+    if (document.getElementById(cellId).textContent == "") {
+      console.log(`Slotid isValidPosition ${cellId} `);
+      return true;
+    } else {
+      console.log(`Slotid --FAIL-- isValidPosition ${cellId} `);
+      return false;
+    }
+  };
+  let win_check = (player) => {
+    console.log(`Started win check for ${player}`);
+    if (
+      (gameBoard.theGameboard[0] == gameBoard.theGameboard[1] &&
+        gameBoard.theGameboard[1] == gameBoard.theGameboard[2] &&
+        gameBoard.theGameboard[0] != "") ||
+      (gameBoard.theGameboard[3] == gameBoard.theGameboard[4] &&
+        gameBoard.theGameboard[4] == gameBoard.theGameboard[5] &&
+        gameBoard.theGameboard[3] != "") ||
+      (gameBoard.theGameboard[6] == gameBoard.theGameboard[7] &&
+        gameBoard.theGameboard[7] == gameBoard.theGameboard[8] &&
+        gameBoard.theGameboard[6] != "") ||
+      (gameBoard.theGameboard[0] == gameBoard.theGameboard[3] &&
+        gameBoard.theGameboard[3] == gameBoard.theGameboard[6] &&
+        gameBoard.theGameboard[0] != "") ||
+      (gameBoard.theGameboard[1] == gameBoard.theGameboard[4] &&
+        gameBoard.theGameboard[4] == gameBoard.theGameboard[7] &&
+        gameBoard.theGameboard[1] != "") ||
+      (gameBoard.theGameboard[2] == gameBoard.theGameboard[5] &&
+        gameBoard.theGameboard[5] == gameBoard.theGameboard[8] &&
+        gameBoard.theGameboard[2] != "") ||
+      (gameBoard.theGameboard[0] == gameBoard.theGameboard[4] &&
+        gameBoard.theGameboard[4] == gameBoard.theGameboard[8] &&
+        gameBoard.theGameboard[0] != "") ||
+      (gameBoard.theGameboard[2] == gameBoard.theGameboard[4] &&
+        gameBoard.theGameboard[4] == gameBoard.theGameboard[6] &&
+        gameBoard.theGameboard[2] != "")
+    ) {
+      gameboardDOM.style.opacity = "0";
+      document.getElementById(
+        "win_block"
+      ).textContent = `${activePlayer.name} wins!`;
+    }
+  };
+  return { round, isValidPosition, win_check };
 })();
 
 var domControl = (() => {
@@ -64,19 +109,27 @@ var domControl = (() => {
   };
   var addMarker = (player, slotId) => {
     if (!gameFlow.isValidPosition(slotId)) {
+      //	Valid position???
       console.log(`Position ${slotId} in not valid. Skipping addMarker`);
     } else {
       document.getElementById(slotId).textContent = player.marker;
-      console.log(
-        `addMarker called for player: ${player.name} (ACTIVE PLAYER: ${activePlayer} at ${slotId}`
-      );
+      gameBoard.theGameboard[slotId[1]] = player.marker;
+      //console.log(gameBoard.theGameboard);
+      //console.log(
+      //  `addMarker called for player: ${player.name} (ACTIVE PLAYER: ${activePlayer} at ${slotId}`
+      //);
+      gameFlow.win_check(player);
       turnCounter++;
+      if (turnCounter == 10) {
+        alert("It's a tie!!!");
+      }
     }
 
-    gameFlow.round(player1, player2);
+    gameFlow.round(gameBoard.playerO, gameBoard.playerX);
   };
   var render = () => {
     domControl.clearScreen();
+
     return {};
   };
   return { addMarker, clearScreen, render };
